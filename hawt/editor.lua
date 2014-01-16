e2120a252c4839f633687d5081f21002d467f876
Bob Somers <bob@bobsomers.com>
2014-01-15 23:35:05 -0800

    Editor font and better key/mouse capture.

--- hawt/editor.lua
+++ hawt/editor.lua
@@ -1,47 +1,67 @@
 local Editor = {}
 
-local hotkey = "pause"
-local active = false
+function Editor:init()
+    self.hotkey = "pause"
+    self.active = false
+    self.font = love.graphics.newFont("hawt/assets/days.ttf", 24)
+end
 
-function Editor.update(stockFunc, dt)
-    if not active then
+function Editor:update(stockFunc, dt)
+    if not self.active then
         stockFunc(dt)
     end
 end
 
-function Editor.draw(stockFunc)
+function Editor:draw(stockFunc)
     stockFunc()
 
-    if active then
-        love.graphics.print("HAWT IS ACTIVE", 50, 50)
+    local ww = love.window.getWidth()
+    local wh = love.window.getHeight()
+
+    if self.active then
+        love.graphics.setColor(0, 0, 0, 128)
+        love.graphics.rectangle("fill", 0, 0, ww, wh)
+
+        love.graphics.setColor(255, 255, 255)
+        love.graphics.setFont(self.font)
+        love.graphics.print("Hawt is active...", 50, 50)
     end
 end
 
-function Editor.keypressed(stockFunc, key, isrepeat)
-    if key == hotkey then
-        active = not active
-    else
-        if active then
-            print("EDITOR PRESSED", key)
-        else
-            stockFunc(key, isrepeat)
-        end
+function Editor:keypressed(stockFunc, key, isrepeat)
+    if not self.active and key ~= self.hotkey then return stockFunc(key, isrepeat) end
+
+    if key == self.hotkey then
+        self.active = not self.active
     end
+
+    print("EDITOR PRESSED", key)
 end
 
-function Editor.keyreleased(stockFunc, key)
-    if key ~= hotkey then
-        if active then
-            print("EDITOR RELEASED", key)
-        else
-            stockFunc(key)
-        end
-    end
+function Editor:keyreleased(stockFunc, key)
+    if not self.active then return stockFunc(key) end
+
+    print("EDITOR RELEASED", key)
+end
+
+function Editor:mousepressed(stockFunc, x, y, button)
+    if not self.active then return stockFunc(x, y, button) end
+end
+
+function Editor:mousereleased(stockFunc, x, y, button)
+    if not self.active then return stockFunc(x, y, button) end
 end
 
 function Editor.registerEvents()
     local function nullFunc() end
-    local callbacks = {"draw", "keypressed", "keyreleased", "update"}
+    local callbacks = {
+        "draw",
+        "keypressed",
+        "keyreleased",
+        "mousepressed",
+        "mousereleased",
+        "update"
+    }
 
     -- Patch callbacks that the editor wants to catch, passing the original
     -- function as a continuation to our hijacked version. This allows the
@@ -50,10 +70,12 @@ function Editor.registerEvents()
         local stockFunc = love[f] or nullFunc
         if Editor[f] then
             love[f] = function(...)
-                return Editor[f](stockFunc, ...)
+                return Editor[f](Editor, stockFunc, ...)
             end
         end
     end
+
+    Editor.init(Editor)
 end
 
 return Editor
