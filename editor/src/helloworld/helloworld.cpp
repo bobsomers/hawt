0cf50f26cc889bbfdcf70b1abad5e240cc725467
Bob Somers <bob@bobsomers.com>
2014-01-20 02:43:57 -0800

    Build enet and link with it.

--- editor/src/helloworld/helloworld.cpp
+++ editor/src/helloworld/helloworld.cpp
@@ -1,9 +1,23 @@
 #include <QApplication>
 #include <QPushButton>
 
+#include <enet/enet.h>
+
+#include <cstdlib>
+#include <iostream>
+
 int main(int argc, char* argv[]) {
     QApplication app(argc, argv);
     QPushButton button("Hello, world!");
+
+    if (enet_initialize() != 0) {
+        std::cerr << "Failed to initialize enet." << std::endl;
+    } else {
+        std::cout << "Enet initialized successfully." << std::endl;
+    }
+    std::atexit(enet_deinitialize);
+
     button.show();
+
     return app.exec();
 }
